013188c0d94e751ec28d956a1311f3e6d54252ee
Muroz <dazc35@mun.ca>
2022-05-01 16:15:14 -0300

    CUIDEV-00005: create text input component

---
+++ src/components/TextInput/TextInput.stories.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import TextInput from "./TextInput";
+
+const Story = {
+  title: "Interactions/TextInput",
+  component: TextInput,
+} as ComponentMeta<typeof TextInput>;
+
+export default Story;
+
+const Template: ComponentStory<typeof TextInput> = (args) => (
+  <TextInput {...args} />
+);
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {};
