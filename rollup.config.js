5bd7b9b17a3281849efc3e4009c3afac4289636f
Muroz <dazc35@mun.ca>
2022-08-27 10:22:28 -0300

    CUI-0004: Updated component exports and added typings

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,13 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-// import dts from "rollup-plugin-dts";
+import dts from "rollup-plugin-dts";
 
 const packageJson = require("./package.json");
 
 export default [
   {
-    input: "src/index.tsx",
+    input: "src/index.ts",
     output: [
       {
         file: packageJson.main,
@@ -26,9 +26,9 @@ export default [
       typescript({ tsconfig: "./tsconfig.json" }),
     ],
   },
-  // {
-  //   input: "./dist/esm/types/index.d.ts",
-  //   output: [{ file: "dist/index.d.ts", format: "esm" }],
-  //   plugins: [dts()],
-  // },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
 ];
---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export * from './Buttons';
+export { default as LegacyButton } from './Button';
+
+export { default as Checkbox } from './Checkbox';
+
+export { default as TextInput } from './TextInput';
+
+export { default as Header } from './Typography/Header';
